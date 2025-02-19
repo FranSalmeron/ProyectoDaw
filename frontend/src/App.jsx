39d37eb8d5d51b2c78757fb485ec13dcc1ca0d54
_Salm3ron_ <fransalmeronpuig123@gmail.com>
2025-02-19 11:23:02 +0100

    functional structure and conecxions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,21 @@
 import { useEffect, useState } from 'react';
-import React from 'react';
-import WebSocketComponent from './components/WebSocketComponent.tsx';
-import './App.css';
+import './App.css'
 
-const urlBackend = import.meta.env.VITE_BACKEND_URL;
-const portBackend = import.meta.env.VITE_BACKEND_PORT;
+const urlApi = import.meta.env.REACT_APP_API_URL;
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    // Fetch para obtener la respuesta del backend
-    fetch(`${urlBackend}/api/db`)
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
-  return (
-    <>
-      <div>
-        <h1>Frontend en React de Francisco José Salmerón Puig</h1>
-        <p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
-        <p>respuesta del Backend: {message || 'Cargando información...'}</p>
-      </div>
-    </>
-  );
+const [message, setMessage] = useState('');
+useEffect(() => {
+fetch(`${urlApi}:8001/api/db`)
+.then((res) => res.json())
+.then((data) => setMessage(data.message));
+}, []);
+return (
+<div>
+<h1>Frontend en React de Francisco José Salmerón Puig</h1>
+<p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
+<p>respuesta del Backend: {message || 'Cargando información...'}</p>
+</div>
+);
 }
-
 export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
-export default ({
-  server: {
-    host: '0.0.0.0',  // Permite que Vite sea accesible desde fuera del contenedor
-    port: 3000,        // O cualquier otro puerto que quieras usar
-  },
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),],
+  server:{
+    host:"0.0.0.0", // configucion para poder trabajar en el entorno de desarrollo real
+    port:3000,
+    // allowedHosts:['frontend']
+  }
+})
+
