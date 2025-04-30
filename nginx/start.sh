52ef6c71fa5a0d5a094ceb5e1f8f5cd74851ecba
_Salm3ron_ <fransalmeronpuig123@gmail.com>
2025-04-30 12:44:58 +0200

    pruebas de conexion 1

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PORT=${PORT:-8081}
+PORT=${PORT:-8080}
 
 # Reemplaza ${PORT} en el archivo de configuración Nginx con el valor real de la variable de entorno PORT
 sed -i "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf
