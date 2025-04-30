f2afb9f527b4d2ede05eef68975dfbaf19a13aa8
_Salm3ron_ <fransalmeronpuig123@gmail.com>
2025-04-30 10:34:32 +0200

    redis changes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,5 +6,10 @@ PORT=${PORT:-8080}
 # Solo modifica VirtualHost, no uses "Listen" (Railway gestiona eso internamente)
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
 
+redis-server --daemonize yes
+
+# Ejecuta migraciones (opcional, solo si quieres que se lancen al iniciar)
+php bin/console doctrine:migrations:migrate --no-interaction
+
 # Iniciar Apache
 exec apache2-foreground
