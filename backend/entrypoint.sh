c1d7fa67c8ddea781259cd37e04658916d7954f1
_Salm3ron_ <fransalmeronpuig123@gmail.com>
2025-04-30 10:48:26 +0200

    subida de db

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,8 +8,8 @@ sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-avail
 
 redis-server --daemonize yes
 
-# Ejecuta migraciones (opcional, solo si quieres que se lancen al iniciar)
-php bin/console doctrine:migrations:migrate --no-interaction
+# Ejecuta la actualizacion de la BD (opcional, solo si quieres que se lancen al iniciar)
+php bin/console doctrine:schema:update --complete --force
 
 # Iniciar Apache
 exec apache2-foreground
