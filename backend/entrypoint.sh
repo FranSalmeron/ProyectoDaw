1b25215b30f2c4647e3f2893f065b94a8c4099ed
_Salm3ron_ <fransalmeronpuig123@gmail.com>
2025-04-30 11:06:37 +0200

    frontend changes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usa el puerto de Railway o default al 8080
-PORT=${PORT:-8080}
+PORT=${PORT:-8081}
 
 # Solo modifica VirtualHost, no uses "Listen" (Railway gestiona eso internamente)
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
