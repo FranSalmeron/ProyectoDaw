f524190dd24eaa37b294fb78546f0128cc850f75
_Salm3ron_ <fransalmeronpuig123@gmail.com>
2025-05-21 12:24:45 +0200

    redimensionado de imagenes

--- frontend/src/helpers/cloudinaryHelper.jsx
+++ frontend/src/helpers/cloudinaryHelper.jsx
@@ -1,5 +1,5 @@
-
-export default function transformCloudinaryUrl(originalUrl, transformation = "w_600,c_fit,f_auto,q_auto") {
-  if (!originalUrl.includes("/upload/")) return originalUrl; // No tocar si no es una URL válida
+export default function transformCloudinaryUrl(originalUrl, transformation = "w_600,h_600,c_fill,f_auto,q_auto") {
+  if (!originalUrl.includes("/upload/")) return originalUrl;
   return originalUrl.replace("/upload/", `/upload/${transformation}/`);
 }
+
